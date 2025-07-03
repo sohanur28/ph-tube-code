ca1ebe9a93fa8c503706ce9c12aef8cd536f39f2
sohanur28 <gsrahman28@gmail.com>
2025-07-03 12:23:15 +0600

    display categories

--- script/video.js
+++ script/video.js
@@ -1,20 +1,36 @@
-console.log('video script added');
+console.log("video script added");
 // 1 - Fetch, Load and Show Categories on html
 
 // create loadCategories
-const loadCategories = () =>{
+const loadCategories = () => {
+  // fetch the data
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories))
+    .catch((error) => console.log(error));
+};
 
-    // fetch the data
-    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then(res => res.json())
-    .then(data => displayCategories(data.categories))
-    .catch(error => console.log(error));
-}
+// {
+//   category: "Music";
+//   category_id: "1001";
+// }
 
 // create displayCategories
-const displayCategories= (data) => {
-    // add Data in html
-    console.log(data);
-}
+const displayCategories = (categories) => {
+  const categoryContainer = document.getElementById('categories');
+
+  // add Data in html
+  categories.forEach((item) => {
+    console.log(item);
+
+    // create a button
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+
+    // add button to category container
+    categoryContainer.append(button);
+  });
+};
 
 loadCategories();
