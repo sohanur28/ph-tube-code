8eed5838bcc508dc610f34ae6e278e330e703045
sohanur28 <gsrahman28@gmail.com>
2025-07-03 18:57:27 +0600

    tailwind setup

--- script/video.js
+++ script/video.js
@@ -1,6 +1,14 @@
-console.log("video script added");
-// 1 - Fetch, Load and Show Categories on html
+// console.log("video script added");
 
+function getTimeString(time){
+    // get Hour and rest seconds
+    const hour = parseInt(time/3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
+// 1 - Fetch, Load and Show Categories on html
 // create loadCategories
 const loadCategories = () => {
   // fetch the data
@@ -52,7 +60,12 @@ const displayVideos = (videos) => {
       src=${video.thumbnail}
       class="h-full w-full object-cover"
       alt="Shoes" />
-      <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${video.others.posted_date}</span>
+      ${
+        video.others.posted_date?.length == 0
+          ? ""
+          : `<span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(video.others.posted_date)}</span>`
+      }
+      
   </figure>
   <div class="px-0 py-2 flex">
     <div>
--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const isVerified = "";
+// const isVerified = "";
 
 // if(isVerified === true){
 //     console.log('user is verified');
@@ -7,4 +7,15 @@ const isVerified = "";
 //     console.log('user is not verified');
 // }
 
-console.log(`${isVerified === true ? "user is verified" : "user is not verified"}`)
+// console.log(`${isVerified === true ? "user is verified" : "user is not verified"}`)
+
+function getTimeString(time){
+    // get Hour and rest seconds
+    const hour = parseInt(time/3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
+
+console.log(getTimeString(7865));
