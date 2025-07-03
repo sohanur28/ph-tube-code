a5ea163750bad23876ba4d04b74f3d47863ae579
sohanur28 <gsrahman28@gmail.com>
2025-07-03 17:30:05 +0600

    render conditional data

---
+++ test.js
@@ -0,0 +1,10 @@
+const isVerified = "";
+
+// if(isVerified === true){
+//     console.log('user is verified');
+// }
+// else{
+//     console.log('user is not verified');
+// }
+
+console.log(`${isVerified === true ? "user is verified" : "user is not verified"}`)
